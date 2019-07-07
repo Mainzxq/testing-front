d34c0c92efd35a00bfa17514713c6f665847c249
Mainzxq <760649@qq.com>
2019-07-08 00:45:51 +0800

    building auth model

---
+++ src/context/auth/AuthState.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import axios from "axios";
+import authContext from "./authContext";
+import authReducer from "./authReducer";
+import { ASK_FOR_AUTH, ASK_FOR_LOGIN } from "../types";
+import { async } from "q";
+
+const AuthState = props => {
+  const initialState = {
+    token: null,
+    user: null,
+    loading: null,
+    isAuthenticated: null
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // 登陆验证
+  const askForLogin = async data => {
+    const res = await axios.post("http://106.13.7.75:3000/login", data);
+    dispatch({ type: ASK_FOR_LOGIN, payload: res });
+  };
+
+  return (
+    <authContext.Provider
+      value={{
+        state,
+        askForLogin
+      }}
+    >
+      {props.children}
+    </authContext.Provider>
+  );
+};
+
+export default AuthState;
